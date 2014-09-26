8a4dee3bb024e11344463e830d868886d27f13a7
Jesse Donat <jdonat@capstonedigital.com>
2014-09-26 15:48:11 -0500

    Init commit

---
+++ src/array.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Given an array, find all the values recursively.
+ *
+ * @param  array $array The Array to be Flattened
+ * @param  bool  $allow_duplicates Should the array allow duplicates
+ * @return array The resulting array or NULL on failure
+ */
+function array_flatten( array $array, $allow_duplicates = false ) {
+	$it    = new RecursiveIteratorIterator(new RecursiveArrayIterator($array));
+	$final = array();
+	foreach( $it as $v ) {
+		if( $allow_duplicates ) {
+			$final[] = $v;
+		} else {
+			$final[$v] = $v;
+		}
+	}
+
+	return $final;
+}
+
+/**
+ * Given an array of arrays, merges the array's children together.
+ *
+ * @param array      $arrays An array of arrays.
+ * @param array|null $keys The merged array.
+ * @return array
+ */
+function array_blend( array $arrays, array $keys = null ) {
+	if( !is_array($arrays) ) return array();
+	$out = array();
+
+	foreach( $arrays as $key => $array ) {
+		if( is_array($array) && (is_null($keys) || (in_array($key, $keys))) ) {
+			foreach( $array as $value ) {
+				$out [] = $value;
+			}
+		}
+	}
+
+	return $out;
+}
+
+/**
+ * Given an array of similarly keyed arrays, returns an array of only the values of the key.
+ *
+ * @param array      $arrays An array of similarly keyed arrays
+ * @param int|string $key the desired key
+ * @return array the flattened array
+ */
+function array_key_array( array $arrays, $key ) {
+	if( !is_array($arrays) ) {
+		return array();
+	}
+	$out = array();
+	foreach( $arrays as $i => $array ) {
+		$out[$i] = $array[$key];
+	}
+
+	return $out;
+}
+
+/**
+ * Given an array of similarly keyed arrays, returns an array of only the selected keys.
+ *
+ * @param array            $arrays
+ * @param array|int|string $keys The key or array of keys to return
+ * @return array
+ */
+function array_keys_array( array $arrays, $keys ) {
+	$keys = (array)$keys;
+
+	$out = array();
+	foreach( $arrays as $i => $array ) {
+		foreach( $keys as $index ) {
+			$out[$i][$index] = $array[$index];
+		}
+	}
+
+	return $out;
+}
+
+/**
+ * Given a keyed array, fills any missing values.
+ *
+ * @param  array $array A Keyed array
+ * @param  array $keys The keys that must exist
+ * @param  mixed $fill The desired value to fill with
+ * @return array
+ */
+function array_key_refill( array $array, $keys, $fill = array() ) {
+	foreach( $keys as $key ) {
+		if( !isset($array[$key]) ) {
+			$array[$key] = $fill;
+		}
+	}
+
+	return $array;
+}
+
